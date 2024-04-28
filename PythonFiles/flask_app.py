8a4164649c0338872ce74c6db32e7150015807db
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-04-28 12:01:55 +0530

    fixed bugs

--- PythonFiles/flask_app.py
+++ PythonFiles/flask_app.py
@@ -1,27 +1,30 @@
 from flask import Flask,render_template,Response,request
 from main import *
-import os
+
 app = Flask(__name__)
 
 
 
 @app.route('/')
 def watch():
-    return render_template('home.html')
+    searchTerm=''
+    src = search(searchTerm)
+    return render_template('series-list.html',src=src,searchTerm=searchTerm)
 
 @app.route('/search' ,methods =('GET','POST'))
 def hello_world():
     searchTerm = request.args.get('q')
+    
     src = search(searchTerm)
     
   
-    return render_template('Search.html',src=src,searchTerm=searchTerm)
+    return render_template('series-list.html',src=src,searchTerm=searchTerm)
 
-@app.route('/mang/<name>',  methods=('GET', 'POST'))
+@app.route('/manga/<name>',  methods=('GET', 'POST'))
 def epi(name):
     src = getepi(name)
     
-    return render_template('epi.html',epi=src,name=name,n=1)
+    return render_template('episode-list.html',epi=src,name=name,n=1)
 
 @app.route("/manga/<li>/<cha>/",  methods=('GET', 'POST'))
 def get_chap(li,cha):
@@ -29,7 +32,7 @@ def get_chap(li,cha):
     
     src = scrapeImg(URL)
     print(URL)
-    return render_template('Chapter.html',src=src,chapter = cha)
+    return render_template('chapter-read.html',src=src,chapter = cha)
     
  
 
